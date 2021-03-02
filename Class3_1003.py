b235068c61f3b700e1f760be9836c67ac38a9184
SuminKim <denny0323@gmail.com>
2021-03-02 15:26:46 +0900

    Add files via upload

---
+++ Class3_1003.py
@@ -0,0 +1,20 @@
+import sys
+T = int(input())
+
+zeros = [1,0]
+ones = [0,1]
+
+for i in range(2,41):
+    zeros.append(zeros[i-1]+zeros[i-2])
+    ones.append(ones[i-1]+ones[i-2])
+
+for _ in range(T):
+    n = int(input())
+    print(zeros[n], ones[n])
+
+'''
+result
+@ Memory : 28776 KB
+@ Time : 72ms
+@ Code length : 226B
+'''
