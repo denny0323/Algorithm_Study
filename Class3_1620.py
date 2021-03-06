c02b91e68516daff73c22e62ceee12c3f38163c2
SuminKim <denny0323@gmail.com>
2021-03-06 21:30:20 +0900

    Add files via upload

---
+++ Class3_1620.py
@@ -0,0 +1,30 @@
+'''
+> Problem
+@ 백준 Class3 1620번
+@ URL https://www.acmicpc.net/problem/1620
+'''
+
+import sys
+N, M = map(int, sys.stdin.readline().split())
+
+Poketmons = []
+Pokedex = {}
+for i in range(1,N+1):
+    poketmon = sys.stdin.readline().rstrip()
+    Pokedex[poketmon] = i
+    Poketmons.append(poketmon)
+    
+for _ in range(M):
+    Q = sys.stdin.readline().rstrip()
+    try: 
+        Q = int(Q)
+        print(Poketmons[Q-1])
+    except:
+        print(Pokedex[Q])   
+
+'''
+> result
+@ Memory : 46916 KB
+@ Time : 320ms
+@ Code length : 372B
+'''
