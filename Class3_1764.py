7c65807274c7c900484a7e7a10599770a1e8f123
SuminKim <denny0323@gmail.com>
2021-03-07 17:16:47 +0900

    Add files via upload

---
+++ Class3_1764.py
@@ -0,0 +1,25 @@
+'''
+> Problem
+@ 백준 Class3 1764번
+@ URL https://www.acmicpc.net/problem/1764
+'''
+
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+
+nolistened = [sys.stdin.readline().strip() for _ in range(N)]
+noseen = [sys.stdin.readline().strip() for _ in range(M)]
+
+intersection = set(nolistened) & set(noseen)
+
+print(len(intersection))
+for i in sorted(intersection):
+    print(i)
+    
+'''
+> result
+@ Memory : 42324 KB
+@ Time : 128ms
+@ Code length : 294B
+'''
